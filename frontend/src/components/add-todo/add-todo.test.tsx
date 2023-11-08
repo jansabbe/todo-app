e65d3279dece545f5e163a535a711a680d2bd83b
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2023-11-08 20:43:45 +0100

    Solution add todo

--- frontend/src/components/add-todo/add-todo.test.tsx
+++ frontend/src/components/add-todo/add-todo.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from "../../test-lib/test-utils";
 import { AddTodoForm } from "./index.tsx";
 
-test("dummy test", () => {
-    render(<AddTodoForm />);
-    const addButton = screen.getByRole("button", { name: /add/i });
-    expect(addButton).toBeDisabled();
+test("clear description after pressing enter", async () => {
+    const { user } = render(<AddTodoForm />);
+    const description = screen.getByRole("textbox", { name: /description/i });
+    await user.type(description, "hello{Enter}");
+    expect(description).toHaveValue("");
+});
+
+test("clear description after pressing add button", async () => {
+    const { user } = render(<AddTodoForm />);
+    const description = screen.getByRole("textbox", { name: /description/i });
+    await user.type(description, "hello");
+    await user.click(screen.getByRole("button", { name: /add/i }));
+    expect(description).toHaveValue("");
 });
