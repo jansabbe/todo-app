a054f6bf1ca7ede14366692f44db6d3377b1587c
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2023-11-08 19:59:44 +0100

    Solution testing library

--- frontend/src/components/footer/footer.test.tsx
+++ frontend/src/components/footer/footer.test.tsx
@@ -1,6 +1,31 @@
 import { render, screen } from "../../test-lib/test-utils";
+import { TodoTO } from "../../lib/api.ts";
+import { faker } from "@faker-js/faker";
+import { Footer } from "./index.tsx";
 
-test("dummy test", () => {
-    render(<div>hello world</div>);
-    expect(screen.getByText("hello world")).toBeInTheDocument();
+function aTodo(overrides: Partial<TodoTO> = {}): TodoTO {
+    return {
+        id: faker.string.uuid(),
+        description: faker.lorem.sentence(),
+        done: faker.datatype.boolean(),
+        ...overrides,
+    };
+}
+
+test('footer shows "All done!" when there are no todos left', () => {
+    const todos = [aTodo({ done: true })];
+    render(<Footer todos={todos} />);
+    expect(screen.getByText("All done!")).toBeInTheDocument();
+});
+
+test('"1 todo left" when there is one', () => {
+    const todos = [aTodo({ done: false })];
+    render(<Footer todos={todos} />);
+    expect(screen.getByText("1 todo left")).toBeInTheDocument();
+});
+
+test('"2 todos left" when there are two', () => {
+    const todos = [aTodo({ done: false }), aTodo({ done: false })];
+    render(<Footer todos={todos} />);
+    expect(screen.getByText("2 todos left")).toBeInTheDocument();
 });
