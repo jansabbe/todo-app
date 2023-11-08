f6d22d6d85922d90dfd8ff2ad4bbf270c7c22bb7
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2023-11-08 19:12:00 +0100

    Solution 1

--- frontend/src/components/add-todo/validate.test.ts
+++ frontend/src/components/add-todo/validate.test.ts
@@ -1,5 +1,25 @@
 import { isValid } from "./validate.ts";
+import { expect } from "vitest";
 
-test("dummy test", () => {
-    expect(1 + 2).toEqual(3);
+test.each(["", " ", "\t"])(
+    "when passing an empty string ('%s'), isValid should return false",
+    (input) => {
+        expect(isValid(input)).toEqual(false);
+    },
+);
+
+test("when passing a string with text, isValid should return true", () => {
+    expect(isValid("run in park")).toEqual(true);
 });
+
+test.each([
+    { nbCharacters: 99, expected: true },
+    { nbCharacters: 100, expected: false },
+    { nbCharacters: 101, expected: false },
+])(
+    "when passing a string with $nbCharacters characters, isValid should return $expected",
+    ({ nbCharacters, expected }) => {
+        const description = Array(nbCharacters).fill("a").join("");
+        expect(isValid(description)).toEqual(expected);
+    },
+);
