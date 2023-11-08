26bad42095199ac8223a1711b155b78ae0c01d42
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2023-11-08 20:38:10 +0100

    Fixing issue with test server

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,7 +1,10 @@
 import "@testing-library/jest-dom/vitest";
 import { mswServer, resetTodos } from "./test-lib/test-server";
+import { setGlobalOrigin } from "undici";
 
 beforeAll(() => {
+    // Workaround for native fetch/msw2.0: https://github.com/mswjs/msw/issues/1625
+    setGlobalOrigin(window.location.href);
     mswServer.listen({ onUnhandledRequest: "error" });
 });
 
