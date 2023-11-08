61f5cb38ee15f0342011efb55a1665d1eb06de85
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2023-11-08 19:45:14 +0100

    Solution test builders

--- frontend/src/components/footer/count.test.ts
+++ frontend/src/components/footer/count.test.ts
@@ -1,5 +1,27 @@
 import { countTodosLeft } from "./count.ts";
+import { expect } from "vitest";
+import { faker } from "@faker-js/faker";
+import { TodoTO } from "../../lib/api.ts";
 
-test("dummy test", () => {
-    expect(1 + 1).toEqual(2);
+function aTodo(overrides: Partial<TodoTO> = {}): TodoTO {
+    return {
+        id: faker.string.uuid(),
+        description: faker.lorem.sentence(),
+        done: faker.datatype.boolean(),
+        ...overrides,
+    };
+}
+
+test("when passing an empty array, countTodosLeft should return 0", () => {
+    expect(countTodosLeft([])).toEqual(0);
+});
+
+test("when passing an array where all todos are done, countTodosLeft should return 0", () => {
+    const todos = [aTodo({ done: true }), aTodo({ done: true })];
+    expect(countTodosLeft(todos)).toEqual(0);
+});
+
+test("when passing an array where 1 todo is done and 2 are not done, countTodosLeft should return 2", () => {
+    const todos = [aTodo({ done: true }), aTodo({ done: false }), aTodo({ done: false })];
+    expect(countTodosLeft(todos)).toEqual(2);
 });
